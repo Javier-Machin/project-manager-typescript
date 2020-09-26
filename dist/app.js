642a32f9cbb043f6c4122a9e7245ef1e64e73499
Javier M <javier_machin_acosta@hotmail.com>
2020-09-26 17:43:32 +0100

    feat: render form

--- dist/app.js
+++ dist/app.js
@@ -3,6 +3,13 @@ class ProjectInput {
     constructor() {
         this.templateElement = document.getElementById('project-input');
         this.hostElement = document.getElementById('app');
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.attach();
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
     }
 }
+const prjInput = new ProjectInput();
 //# sourceMappingURL=app.js.map
